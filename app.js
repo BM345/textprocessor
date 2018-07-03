ddbb212b7819ff53d667fa5d1687c37ba9fb7845
Ben Milnes <benjamin.milnes@nagwa.com>
2018-07-03 15:43:36 +0100

    Slightly adjusted algorithm

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.controller("MainController", ["$scope", function MainController($scope) {
   $scope.processText = function(text) {
 
     text = text.replace(/\n/g, " ");
-    text = text.replace(/([0-9]*\.?[0-9]+)-(g|kg|m|km)/g, "$1 $2");
+    text = text.replace(/([0-9]*\.?[0-9]+)-(g|kg|m|km|N)/g, "$1 $2");
     text = text.replace(/\(([a-z])\)/g, "\n\n$1.)\n\n");
     text = text.replace(/\n /g, "\n");
 
